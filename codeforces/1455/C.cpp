59fd89050ab39658f59c46fbe0b91fadcddf5eab
shivang <shivangraina70@gmail.com>
2020-11-30 21:42:00 +0530

    Add solution for problem `C - Ping-pong` Link: https://codeforces.com/contest/1455/problem/C Tags: constructive algorithms, games, math, *1100 Ref: https://codeforces.com/contest/1455/submission/100042605

---
+++ codeforces/1455/C.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+#include <iostream>
+#define ll long long
+using namespace std;
+ll Ceil(ll a, ll b) { return ((a / b) + (a % b != 0)); }
+#define MAXX 10000000000000
+
+int main() {
+
+  ll t;
+  cin >> t;
+  while (t--) {
+  ll x,y;
+  cin>>x>>y;
+   cout<<x-1<<" "<<y<<endl;
+  }
+}
+
+
