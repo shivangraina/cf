777f77144d4bfcb3c5caf1d45351ef5a058aa7b4
shivang <shivangraina70@gmail.com>
2020-11-30 20:14:00 +0530

    Add solution for problem `A - Strange Functions` Link: https://codeforces.com/contest/1455/problem/A Tags: math, number theory, *800 Ref: https://codeforces.com/contest/1455/submission/100001970

---
+++ codeforces/1455/A.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+#include <iostream>
+#define ll long long
+using namespace std;
+ll Ceil(ll a, ll b) { return ((a / b) + (a % b != 0)); }
+#define MAXX 10000000000000
+
+int main() {
+
+  ll t;
+  cin >> t;
+  while (t--) {
+    string n;
+    cin>>n;
+    
+    
+    cout<<n.length()<<"\n";
+  }
+}
+
+
